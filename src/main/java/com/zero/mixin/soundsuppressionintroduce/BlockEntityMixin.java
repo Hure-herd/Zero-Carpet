83703090018b8883bce4a896360b01c470a9f602
紫薯薯薯薯桑 <1464234368q@gmail.com>
2025-05-15 14:59:23 +0800

    add rule soundsuppression

--- src/main/java/com/zero/mixin/soundsuppressionintroduce/BlockEntityMixin.java
+++ src/main/java/com/zero/mixin/soundsuppressionintroduce/BlockEntityMixin.java
@@ -7,9 +7,19 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.CalibratedSculkSensorBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockEntity.class)
 public abstract class BlockEntityMixin{
+
+    @Inject(method = "validateSupports", at = @At("HEAD"), cancellable = true)
+    private void allowInvalidBlockEntities(BlockState blockState, CallbackInfo ci) {
+        if (ZeroSettings.soundsuppression) {
+            ci.cancel();
+        }
+    }
+
     @WrapWithCondition(
             method = "<init>",
             at = @At(
