e0693005219c195ae15766ee07a7aefb765dbee6
Crystal0404 <syh040325@163.com>
2025-08-08 01:15:33 +0800

    fix mergeTNTPro
    This reverts commit b27b115840df421ac2276de86f14f1cf88d3f85c.

--- src/main/java/com/zero/mixin/TntEntityMixin.java
+++ src/main/java/com/zero/mixin/TntEntityMixin.java
@@ -13,48 +13,49 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
+// I don't know why, everything here is so chaotic that I can't understand it
 @Mixin(TntEntity.class)
-public abstract class TntEntityMixin extends Entity implements TntEntityInterface {
-    @Shadow
-    public abstract int getFuse();
+public abstract class TntEntityMixin extends Entity implements TntEntityInterface
+{
+    @Shadow public abstract int getFuse();
 
-    @Unique
     private TNTLogHelper logHelper;
-
-    @Unique
     private boolean mergeBool = false;
-
-    @Unique
     private int mergedTNT = 1;
 
-    public TntEntityMixin(EntityType<?> entityType_1, World world_1) {
+    public TntEntityMixin(EntityType<?> entityType_1, World world_1)
+    {
         super(entityType_1, world_1);
     }
 
 
     @Inject(method = "<init>(Lnet/minecraft/world/World;DDDLnet/minecraft/entity/LivingEntity;)V",
             at = @At("RETURN"))
-    private void modifyTNTAngle(World world, double x, double y, double z, LivingEntity entity, CallbackInfo ci) {
+    private void modifyTNTAngle(World world, double x, double y, double z, LivingEntity entity, CallbackInfo ci)
+    {
         if (CarpetSettings.hardcodeTNTangle != -1.0D)
             setVelocity(-Math.sin(CarpetSettings.hardcodeTNTangle) * 0.02, 0.2, -Math.cos(CarpetSettings.hardcodeTNTangle) * 0.02);
     }
 
     @Inject(method = "<init>(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/World;)V", at = @At("RETURN"))
-    private void initTNTLoggerPrime(EntityType<? extends TntEntity> entityType_1, World world_1, CallbackInfo ci) {
-        if (LoggerRegistry.__tnt && !world_1.isClient) {
+    private void initTNTLoggerPrime(EntityType<? extends TntEntity> entityType_1, World world_1, CallbackInfo ci)
+    {
+        if (LoggerRegistry.__tnt && !world_1.isClient)
+        {
             logHelper = new TNTLogHelper();
         }
     }
 
     @Inject(method = "tick", at = @At("HEAD"))
-    private void initTracker(CallbackInfo ci) {
-        if (LoggerRegistry.__tnt && logHelper != null && !logHelper.initialized) {
+    private void initTracker(CallbackInfo ci)
+    {
+        if (LoggerRegistry.__tnt && logHelper != null && !logHelper.initialized)
+        {
             logHelper.onPrimed(getX(), getY(), getZ(), getVelocity());
         }
     }
@@ -63,8 +64,9 @@ public abstract class TntEntityMixin extends Entity implements TntEntityInterfac
     @Inject(method = "<init>(Lnet/minecraft/world/World;DDDLnet/minecraft/entity/LivingEntity;)V",
             at = @At(value = "RETURN"))
     private void initTNTLogger(World world_1, double double_1, double double_2, double double_3,
-                               LivingEntity livingEntity_1, CallbackInfo ci) {
-        if (CarpetSettings.tntPrimerMomentumRemoved)
+                               LivingEntity livingEntity_1, CallbackInfo ci)
+    {
+        if(CarpetSettings.tntPrimerMomentumRemoved)
             this.setVelocity(new Vec3d(0.0, 0.20000000298023224D, 0.0));
     }
 
@@ -74,7 +76,7 @@ public abstract class TntEntityMixin extends Entity implements TntEntityInterfac
             logHelper.onExploded(getX(), getY(), getZ(), this.getWorld().getTime());
 
         if (mergedTNT > 1)
-            for (int i = mergedTNT; i < mergedTNT - 1; i++) {
+            for (int i = mergedTNT; i < mergedTNT - 1; i++){
                 this.getWorld().createExplosion(this, this.getX(), this.getBodyY(0.0625),
                         this.getZ(), 4.0F, World.ExplosionSourceType.TNT);
             }
@@ -83,18 +85,19 @@ public abstract class TntEntityMixin extends Entity implements TntEntityInterfac
     @Inject(method = "tick", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/entity/TntEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V",
             ordinal = 1))
-    private void tryMergeTnT(CallbackInfo ci) {
-        if (ZeroSettings.mergeTNTPro) {
+    private void tryMergeTnT(CallbackInfo ci)
+    {
+        if(ZeroSettings.mergeTNTPro){
             Vec3d velocity = getVelocity();
-            if (!getEntityWorld().isClient && mergeBool && velocity.x == 0 && velocity.y == 0 && velocity.z == 0) {
+            if(!getEntityWorld().isClient && mergeBool && velocity.x == 0 && velocity.y == 0 && velocity.z == 0){
                 mergeBool = false;
-                for (Entity entity : getEntityWorld().getOtherEntities(this, this.getBoundingBox())) {
-                    if (entity instanceof TntEntity && !entity.isRemoved()) {
-                        TntEntity entityTNTPrimed = (TntEntity) entity;
+                for(Entity entity : getEntityWorld().getOtherEntities(this, this.getBoundingBox())){
+                    if(entity instanceof TntEntity && !entity.isRemoved()){
+                        TntEntity entityTNTPrimed = (TntEntity)entity;
                         Vec3d tntVelocity = entityTNTPrimed.getVelocity();
-                        if (tntVelocity.x == 0 && tntVelocity.y == 0 && tntVelocity.z == 0
+                        if(tntVelocity.x == 0 && tntVelocity.y == 0 && tntVelocity.z == 0
                                 && this.getX() == entityTNTPrimed.getX() && this.getZ() == entityTNTPrimed.getZ() && this.getY() == entityTNTPrimed.getY()
-                                && getFuse() == entityTNTPrimed.getFuse() + 1) {
+                                && getFuse() == entityTNTPrimed.getFuse() +1){
                             mergedTNT += ((TntEntityInterface) entityTNTPrimed).getMergedTNT();
                             entityTNTPrimed.remove(RemovalReason.DISCARDED);
                         }
@@ -107,9 +110,10 @@ public abstract class TntEntityMixin extends Entity implements TntEntityInterfac
     @Inject(method = "tick", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/entity/TntEntity;setFuse(I)V",
             ordinal = 0))
-    private void setMergeable(CallbackInfo ci) {
+    private void setMergeable(CallbackInfo ci)
+    {
         Vec3d velocity = getVelocity();
-        if (!getEntityWorld().isClient && (velocity.y != 0 || velocity.x != 0 || velocity.z != 0)) {
+        if(!getEntityWorld().isClient && (velocity.y != 0 || velocity.x != 0 || velocity.z != 0)){
             mergeBool = true;
         }
     }
